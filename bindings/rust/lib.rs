d74df57f0ce7be5a6bb8d16f06b8b62a58d757ea
SegaraRai <SegaraRai@users.noreply.github.com>
2025-12-26 20:50:46 +0900

    chore: update dependencies

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_astro::language()).expect("Error loading astro grammar");
+//! parser.set_language(&tree_sitter_astro::language()).expect("Error loading astro grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -17,7 +17,7 @@
 
 use tree_sitter::Language;
 
-extern "C" {
+unsafe extern "C" {
     fn tree_sitter_astro() -> Language;
 }
 
@@ -35,10 +35,10 @@ pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 // Uncomment these to include any queries that this grammar contains
 
-pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
-pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
+// pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
+// pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
@@ -46,7 +46,7 @@ mod tests {
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(super::language())
+            .set_language(&super::language())
             .expect("Error loading astro grammar");
     }
 }
